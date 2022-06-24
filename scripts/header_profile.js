a9a896e3da89bbfa6e9da916480c84001909b0cc
JorgeToAn <jorgetosta.f@gmail.com>
2022-06-23 19:35:32 -0700

    Added scripts

---
+++ scripts/header_profile.js
@@ -0,0 +1,9 @@
+let username = "FUNKTOAST";
+let userid = "9959";
+let numberViews = 3;
+
+document.write(`
+    <div class="container">
+        <p>${numberViews} views</p>
+        <h1>${username}<span>#${userid}</span></h1>
+    </div>`);
---
+++ scripts/main-overview.js
@@ -0,0 +1,111 @@
+let userRank = "Iron";
+let userDivision = 3;
+let playtime = "3h 33m";
+let matchesWon = 4;
+let matchesLost = 2;
+let matchesPlayed = matchesWon+matchesLost;
+let kills = 60;
+let deaths = 84;
+let assists = 33;
+let kdRatio = kills/deaths;
+let kadRatio = (kills+assists)/deaths;
+let damagePerRound = 102.7;
+let headshots = 26;
+let headHits = 35;
+let totalHits = 286;
+let scoreRound = 155.5;
+let killsRound = 0.5;
+let firstBloods = 7;
+let aces = 0;
+let clutches = 15;
+let flawless = 4;
+let mostKills = 19;
+
+document.write(`
+    <div class="title">
+        <h2>E4: A3 Competitive Overview</h2>
+        <p>${playtime} Play Time</p>
+        <p>${matchesPlayed} Matches</p>
+    </div>
+    <div class="stats-highlight">
+        <div class="rating">
+            <h3>Rating</h3>
+            <p>${userRank} ${userDivision}</p>
+        </div>
+        <div class="kad-ratio">
+            <h3>KAD Ratio</h3>
+            <p>${kadRatio}</p>
+        </div>
+        <div class="wins-losses"
+            <p>${matchesWon} W</p>
+            <p>${matchesLost} L</p>
+        </div>
+    </div>
+    <div class="boxed-stats">
+        <div class="box">
+            <h3>Damage/Round</h3>
+            <p>${damagePerRound}</p>
+        </div>
+        <div class="box">
+            <h3>K/D Ratio</h3>
+            <p>${kdRatio}</p>
+        </div>
+        <div class="box">
+            <h3>Headshot%</h3>
+            <p>${headHits/totalHits}%</p>
+        </div>
+        <div class="box">
+            <h3>Win %</h3>
+            <p>${matchesWon/matchesPlayed}</p>
+        </div>
+    </div>
+    <div class="other-stats">
+        <div>
+            <h3>Wins</h3>
+            <p>${matchesWon}</p>
+        </div>
+        <div>
+            <h3>Kills</h3>
+            <p>${kills}</p>
+        </div>
+        <div>
+            <h3>Headshots</h3>
+            <p>${headshots}</p>
+        </div>
+        <div>
+            <h3>Deaths</h3>
+            <p>${deaths}</p>
+        </div>
+        <div>
+            <h3>Assists</h3>
+            <p>${assists}</p>
+        </div>
+        <div>
+            <h3>Score/Round</h3>
+            <p>${scoreRound}</p>
+        </div>
+        <div>
+            <h3>Kills/Round</h3>
+            <p>${killsRound}</p>
+        </div>
+        <div>
+            <h3>First Bloods</h3>
+            <p>${firstBloods}</p>
+        </div>
+        <div>
+            <h3>Aces</h3>
+            <p>${aces}</p>
+        </div>
+        <div>
+            <h3>Clutches</h3>
+            <p>${clutches}</p>
+        </div>
+        <div>
+            <h3>Flawless</h3>
+            <p>${flawless}</p>
+        </div>
+        <div>
+            <h3>Most Kills (Match)</h3>
+            <p>${mostKills}</p>
+        </div>
+    </div>`);
